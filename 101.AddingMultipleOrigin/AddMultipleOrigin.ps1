abb16f8cdbc6b9aea16fbba2288b3a3312a5bb8b
DellLaptop <venkat.muthu@gmail.com>
2020-11-05 00:52:03 +0000

    misc file updated

--- 101.AddingMultipleOrigin/AddMultipleOrigin.ps1
+++ 101.AddingMultipleOrigin/AddMultipleOrigin.ps1
@@ -1,2 +1,7 @@
 # Add one more origin, so a push can be pushed to multiple origins
-git remote add origin git@github.com:Venkat-Muthu/git-useful-commands-1.git
+git remote add origin1 git@github.com:Venkat-Muthu/git-useful-commands-1.git
+
+git remote set-url --add --push all git@github.com:Venkat-Muthu/git-useful-commands.git
+git remote set-url --add --push all git@github.com:Venkat-Muthu/git-useful-commands-1.git
+
+git remote -v
